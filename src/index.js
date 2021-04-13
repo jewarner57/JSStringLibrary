7e9c1d6897327afcc3d09ffd58dd9c618c3025b0
Jonathan Warner <jewarner57@gmail.com>
2021-04-13 10:43:17 -0400

    fix: linting

--- src/index.js
+++ src/index.js
@@ -1,126 +1,125 @@
-const ignoredWords = new Set(["the", "in", "a", "an", "and", "but", "for", "at", "by", "from"])
+
+const ignoredWords = new Set(['the', 'in', 'a', 'an', 'and', 'but', 'for', 'at', 'by', 'from']);
 
 // C1: Uppercase the first char in a string
 String.prototype.capitalize = function () {
   if (this.isEmpty()) {
-    return String(this)
+    return String(this);
   }
 
-  return this[0].toUpperCase() + this.slice(1)
-}
+  return this[0].toUpperCase() + this.slice(1);
+};
 
-// C2: Uppercase all characters 
+// C2: Uppercase all characters
 String.prototype.upper = function () {
-  return this.toUpperCase()
-}
+  return this.toUpperCase();
+};
 
 // C3: Capitalize all words in a string
 String.prototype.capitalizeWords = function () {
-  let words = this.split(' ')
+  const words = this.split(' ');
 
   words.forEach((word, index) => {
-    words[index] = word.capitalize()
-  })
+    words[index] = word.capitalize();
+  });
 
-  return words.join(" ")
-}
+  return words.join(' ');
+};
 
 // C3A: Capitalize all words in a string except certain words
 String.prototype.capitalizeHeadline = function () {
-  let words = this.split(' ')
+  const words = this.split(' ');
 
   words.forEach((word, index) => {
     if (!ignoredWords.has(word) || index === 0) {
-      words[index] = word.capitalize()
+      words[index] = word.capitalize();
     }
-  })
+  });
 
-  return words.join(" ")
-}
+  return words.join(' ');
+};
 
-/* C4A:  Removes all spaces from the beginning and end of a String along with any extra spaces in the middle.
-If more than one space appears in the middle of a string it is replaced by a single space. */
+/* C4A:  Removes all spaces from the beginning and end of a String along with
+any extra spaces in the middle. If more than one space appears in the middle
+of a string it is replaced by a single space. */
 String.prototype.removeExtraSpaces = function () {
-  let str = this.trim()
+  let str = this.trim();
 
   // I didn't think the whitespace (\s) flag would catch
   // everything, but it did and that made things much easier.
 
   // \s (whitespace chars), + (atleast one), /gm (global multiline)
-  str = str.split(/\s+/gm)
+  str = str.split(/\s+/gm);
 
-  return str.join(' ')
-}
-
+  return str.join(' ');
+};
 
 // C5: lowercase string, remove extra spaces, replace spaces with -
 String.prototype.kebabCase = function () {
-  let str = this.toLowerCase().removeExtraSpaces().split(" ").join("-")
+  const str = this.toLowerCase().removeExtraSpaces().split(' ').join('-');
 
-  return str
-}
+  return str;
+};
 
 // C6: lowercase string, remove extra spaces, replace spaces with _
 String.prototype.snakeCase = function () {
-  let str = this.toLowerCase().removeExtraSpaces().split(" ").join("_")
+  const str = this.toLowerCase().removeExtraSpaces().split(' ').join('_');
 
-  return str
-}
+  return str;
+};
 
 // C7: converts the string to camel case
 String.prototype.camelCase = function () {
-  let words = this.removeExtraSpaces().split(' ')
+  const words = this.removeExtraSpaces().split(' ');
 
   words.forEach((word, index) => {
     if (index === 0) {
-      words[index] = word.toLowerCase()
+      words[index] = word.toLowerCase();
     }
     else {
-      words[index] = word.toLowerCase().capitalize()
+      words[index] = word.toLowerCase().capitalize();
     }
-  })
+  });
 
-  return words.join("")
-}
+  return words.join('');
+};
 
 // C8: shift the first letter of the string to the end of the word
 String.prototype.shift = function () {
   if (this.isEmpty()) {
-    return String(this)
+    return String(this);
   }
 
-  let str = this
-  return str.slice(1) + str[0]
-}
+  const str = this;
+  return str.slice(1) + str[0];
+};
 
 // C9: choose the 3 longest words and put a hashtag before them
 String.prototype.makeHashTag = function () {
   if (this.isEmpty()) {
-    return String(this)
+    return String(this);
   }
 
-  let str = this.removeExtraSpaces()
-  str = str.split(" ")
-  let resultLength = str.length < 3 ? str.length : 3;
-  let resultArr = []
+  let str = this.removeExtraSpaces();
+  str = str.split(' ');
+  const resultLength = str.length < 3 ? str.length : 3;
+  const resultArr = [];
 
   str = str.sort((a, b) => {
     if (a.length > b.length) {
-      return -1
+      return -1;
     }
-    else {
-      return 0
-    }
-  }).slice(0, resultLength)
+    return 0;
+  }).slice(0, resultLength);
 
-  str.map((word) => resultArr.push(`#${word}`))
+  str.map((word) => resultArr.push(`#${word}`));
 
-  return resultArr
-}
+  return resultArr;
+};
 
 // C10: Returns true if the given string is empty or contains only whitespace.
 String.prototype.isEmpty = function () {
-  let str = this
+  const str = this;
   // \S (non whitespace chars), + (atleast one), /gm (global multiline)
-  return !new RegExp(/\S+/gm).test(str)
-}
+  return !new RegExp(/\S+/gm).test(str);
+};
--- src/main.test.js
+++ src/main.test.js
@@ -1,81 +1,81 @@
-const as = require('./index.js')
+const as = require('./index.js');
 
 // Test Strings
-const empty = ""
+const empty = '';
 
 test('Capitalize', () => {
-  expect(empty.capitalize()).toBe("")
-  expect("a sentence".capitalize()).toBe("A sentence")
-})
+  expect(empty.capitalize()).toBe('');
+  expect('a sentence'.capitalize()).toBe('A sentence');
+});
 
 test('Upper', () => {
-  expect(empty.upper()).toBe("")
-  expect("bacon".upper()).toBe("BACON")
-})
+  expect(empty.upper()).toBe('');
+  expect('bacon'.upper()).toBe('BACON');
+});
 
 test('Capitalize Words', () => {
-  expect(empty.capitalizeWords()).toBe("")
-  expect("these are some words".capitalizeWords()).toBe("These Are Some Words")
-  expect("these-are_some  words".capitalizeWords()).toBe("These-are_some  Words")
-})
+  expect(empty.capitalizeWords()).toBe('');
+  expect('these are some words'.capitalizeWords()).toBe('These Are Some Words');
+  expect('these-are_some  words'.capitalizeWords()).toBe('These-are_some  Words');
+});
 
 test('Capitalize Headline', () => {
-  expect(empty.capitalizeHeadline()).toBe("")
-  expect("a headline for a magazine".capitalizeHeadline()).toBe("A Headline for a Magazine")
-  expect("another  headline    for    a    magazine".capitalizeHeadline()).toBe("Another  Headline    for    a    Magazine")
-})
+  expect(empty.capitalizeHeadline()).toBe('');
+  expect('a headline for a magazine'.capitalizeHeadline()).toBe('A Headline for a Magazine');
+  expect('another  headline    for    a    magazine'.capitalizeHeadline()).toBe('Another  Headline    for    a    Magazine');
+});
 
 test('Remove Extra Spaces', () => {
-  expect(empty.removeExtraSpaces()).toBe("")
-  expect("\n \t \n\t \r\n\t".removeExtraSpaces()).toBe("")
-  expect("a  word\n for the  de \t\tspacer".removeExtraSpaces()).toBe("a word for the de spacer")
-  expect("      \t\t\t\t\t\t\r\r\r\r   I Hate  \r\t \r \t   White    \n\n      Space \n       ".removeExtraSpaces()).toBe("I Hate White Space")
-})
+  expect(empty.removeExtraSpaces()).toBe('');
+  expect('\n \t \n\t \r\n\t'.removeExtraSpaces()).toBe('');
+  expect('a  word\n for the  de \t\tspacer'.removeExtraSpaces()).toBe('a word for the de spacer');
+  expect('      \t\t\t\t\t\t\r\r\r\r   I Hate  \r\t \r \t   White    \n\n      Space \n       '.removeExtraSpaces()).toBe('I Hate White Space');
+});
 
 test('Kebab Case', () => {
-  expect(empty.kebabCase()).toBe("")
-  expect("kebab me".kebabCase()).toBe("kebab-me")
-  expect("put    these words    on a stick".kebabCase()).toBe("put-these-words-on-a-stick")
-})
+  expect(empty.kebabCase()).toBe('');
+  expect('kebab me'.kebabCase()).toBe('kebab-me');
+  expect('put    these words    on a stick'.kebabCase()).toBe('put-these-words-on-a-stick');
+});
 
 test('Snake Case', () => {
-  expect(empty.snakeCase()).toBe("")
-  expect("imma snek".snakeCase()).toBe("imma_snek")
-  expect("   eat    some    apples   ".snakeCase()).toBe("eat_some_apples")
-})
+  expect(empty.snakeCase()).toBe('');
+  expect('imma snek'.snakeCase()).toBe('imma_snek');
+  expect('   eat    some    apples   '.snakeCase()).toBe('eat_some_apples');
+});
 
 test('Snake Case', () => {
-  expect(empty.camelCase()).toBe("")
-  expect("imma camel".camelCase()).toBe("immaCamel")
-  expect("   eat    some    apples   ".camelCase()).toBe("eatSomeApples")
-})
+  expect(empty.camelCase()).toBe('');
+  expect('imma camel'.camelCase()).toBe('immaCamel');
+  expect('   eat    some    apples   '.camelCase()).toBe('eatSomeApples');
+});
 
 test('Camel Case', () => {
-  expect(empty.camelCase()).toBe("")
-  expect("imma camel".camelCase()).toBe("immaCamel")
-  expect("   eat    some    straw   ".camelCase()).toBe("eatSomeStraw")
-})
+  expect(empty.camelCase()).toBe('');
+  expect('imma camel'.camelCase()).toBe('immaCamel');
+  expect('   eat    some    straw   '.camelCase()).toBe('eatSomeStraw');
+});
 
 test('Shift', () => {
-  expect(empty.shift()).toBe("")
-  expect("cat".shift()).toBe("atc")
-  expect("abrac adabra".shift()).toBe("brac adabraa")
-  expect(" shift these wordies".shift()).toBe("shift these wordies ")
-})
+  expect(empty.shift()).toBe('');
+  expect('cat'.shift()).toBe('atc');
+  expect('abrac adabra'.shift()).toBe('brac adabraa');
+  expect(' shift these wordies'.shift()).toBe('shift these wordies ');
+});
 
 test('Make Hashtag', () => {
-  expect(empty.makeHashTag()).toBe("")
-  expect("\n\n \t\t".makeHashTag()).toBe("\n\n \t\t")
-  expect("hash".makeHashTag()[0]).toBe("#hash")
-  expect("hash tag".makeHashTag()[0]).toBe("#hash")
-  expect("hash tag".makeHashTag()[1]).toBe("#tag")
-  expect("picnic basket extravaganza party for 7".makeHashTag()).toEqual(["#extravaganza", "#picnic", "#basket"])
-})
+  expect(empty.makeHashTag()).toBe('');
+  expect('\n\n \t\t'.makeHashTag()).toBe('\n\n \t\t');
+  expect('hash'.makeHashTag()[0]).toBe('#hash');
+  expect('hash tag'.makeHashTag()[0]).toBe('#hash');
+  expect('hash tag'.makeHashTag()[1]).toBe('#tag');
+  expect('picnic basket extravaganza party for 7'.makeHashTag()).toEqual(['#extravaganza', '#picnic', '#basket']);
+});
 
 test('Is Empty?', () => {
-  expect(empty.isEmpty()).toBe(true)
-  expect(" ".isEmpty()).toBe(true)
-  expect("\t\b\h".isEmpty()).toBe(false)
-  expect("\r\t \n \n \t ".isEmpty()).toBe(true)
-  expect(" w\n\n\n or d".isEmpty()).toBe(false)
-})
+  expect(empty.isEmpty()).toBe(true);
+  expect(' '.isEmpty()).toBe(true);
+  expect('\t\b\h'.isEmpty()).toBe(false);
+  expect('\r\t \n \n \t '.isEmpty()).toBe(true);
+  expect(' w\n\n\n or d'.isEmpty()).toBe(false);
+});
